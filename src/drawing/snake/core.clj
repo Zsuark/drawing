5830cbe27b6b1ad1ec037bb0cc0d4210a09f3f51
Raphael Krausz <krauszraphael@gmail.com>
2018-01-30 12:51:10 +0100

    Cleaned up snake code

--- src/drawing/snake/core.clj
+++ src/drawing/snake/core.clj
@@ -74,18 +74,18 @@
 
 (defn generate-wall
   "Generates a wall that covers the border, except for where the holes are.
-  Returns a list of wall coordinates"
+  Returns a set of wall coordinates"
   []
   (let [border (get-border)
         holes  (generate-holes)]
-    (filter #(not (contains? holes %)) border)))
+    (set (filter #(not (contains? holes %)) border))))
 
 (defn init-state []
   (let [direction     (random-direction)
         initial-state {
-                       :snake (list [
-                                     (- (/ (q/width) 2)  (* (rand-int 2) 10))
-                                     (- (/ (q/height) 2) (* (rand-int 2) 10)) ])
+                       :snake (list
+                                [(- (/ (q/width) 2)  (* (rand-int 2) 10))
+                                 (- (/ (q/height) 2) (* (rand-int 2) 10)) ])
                        
                        :direction      direction
                        :last-direction direction
@@ -97,23 +97,20 @@
     ; (println "initial-state:" initial-state)
     initial-state))
 
-
-(defn add-apple [state really-add]
-  (if really-add
-    (let [old-apples (set (:apples state))
-          wall       (set (:wall state))
-          snake      (set (:snake state))]
-      (loop []
-        (let [x (* (rand-int (/ (q/width)  10)) 10)
-              y (* (rand-int (/ (q/height) 10)) 10)
-              new-apple [x y]]
-          (if (or
-                (contains? old-apples new-apple)
-                (contains? wall new-apple)
-                (contains? snake new-apple))
-            (recur)
-            (update-in state [:apples] #(conj % new-apple))))))
-    state))
+(defn add-apple [state]
+  (let [old-apples (:apples state)
+        wall       (:wall state)
+        snake      (set (:snake state))]
+    (loop []
+      (let [x (* (rand-int (/ (q/width)  10)) 10)
+            y (* (rand-int (/ (q/height) 10)) 10)
+            new-apple [x y]]
+        (if (or
+              (contains? old-apples new-apple)
+              (contains? wall new-apple)
+              (contains? snake new-apple))
+          (recur)
+          (update-in state [:apples] #(conj % new-apple)))))))
 
 
 
@@ -126,7 +123,7 @@
   (q/stroke-weight 2)
   (q/fill 0xff7bcecc)
   (q/text-font (q/create-font "Arial-BoldMT" 10))
-  (add-apple (init-state) true))
+  (add-apple (init-state)))
 
 
 ; draws state it is given to the screen
@@ -138,12 +135,12 @@
   ; background fill
   (q/background 0xfffef8e6)
   
+  
   ; Draw the wall
   (q/with-stroke
     [0xff7bcecc]
     (doseq [[x y] (:wall state)]
       (q/rect x y 10 10)))
-  
   
   
   ; Write the score and draw the apples
@@ -169,7 +166,7 @@
     [0xff3090a1]
     (let [[x y] (first (:snake state))]
       (q/rect x y 10 10 4)))
-  
+    
   ; then the body
   (doseq [[x y] (rest (:snake state))]
     (q/rect x y 10 10 2)))
@@ -193,98 +190,104 @@
            (= new-direction :up)))))
 
 
+(defn update-apple-state
+  "
+  Checks if an apple has been eaten.
+  Updates score and removes eaten apple if needed.
+  Updates the high score if needed.
+  Every third apple resets the wall (and where the hole is)
+  Returns state which has been updated as needed.
+  "
+  [state snake-head]
+  (if (contains? (:apples state) snake-head)
+    (let [new-state (-> state
+                        (update :apples  #(disj % snake-head))
+                        (update :growing #(+ 3 %))
+                        (update :score   inc))
+          new-score (:score new-state)]
+      
+      (if (> new-score @high-score)
+        (reset! high-score new-score))
+      
+      ; (println "update-apple-state old state:" state)
+      ; (println "update-apple-state new-state:" new-state)
+      
+      (if (zero? (mod (:score new-state) 3))
+        (assoc new-state :wall (generate-wall))
+        new-state))
+    state))
+
+(defn move-snake
+  "
+  Called when snake makes a successful move.
+  Updates and returns state.
+  
+  If there are no apples, then there is a 1 in 5 chance of adding one.
+  There are apples, then there is a 1 in (number of apples * 100) chance of adding one.
+  "
+  [state snake-head snake-tail]
+  (let [apple-count  (count (:apples state))
+        adding-apple (zero?
+                       (rand-int (if (zero? apple-count) 5 (* 100 apple-count))))
+        new-state    (-> (if adding-apple
+                           (add-apple state)
+                           state)
+                         (assoc :snake (cons snake-head snake-tail)))]
+    ; (println "move-snake (old) state:" state)
+    ; (println "move-snake   new-state:" new-state)
+    new-state))
+
+
+
 ; gives new state from state passed in
 ; Accepts current state
 ; Gives new state
-(defn update-state [state]
-  ; (println "snake:" (:snake state))
-  ; (println "direction:" (:direction state))
-  ; (println "last-direction:" (:last-direction state))
+(defn update-state
+  "
+  Checks if the snake has collided with itself or the wall
+  If so, reset the game
+  
+  Otherwise:
+  - amend the growth value if needed
+  - update the last-direction to be the now current direction
+  - check the apple situation
+  - move the snakes position
+  "
+  [state]
+  ; (println "state:" state)
   (let [direction       (:direction state)
         [head-x head-y] (first (:snake state))
-        new-x (case direction
-                :left  (mod (- head-x 10) (q/width))
-                :right (mod (+ head-x 10) (q/width))
-                head-x)
-        new-y (case direction
-                :up   (mod (- head-y 10) (q/height))
-                :down (mod (+ head-y 10) (q/height))
-                head-y)
-        
-        new-head [new-x new-y]
-        
-        is-growing (> (:growing state) 0)
-        
-        apples (:apples state)
-        
-        ; if there are no apples there is a
-        ; 1 in 5 chance that we will add one
-        adding-apple (and
-                       (zero? (count apples))
-                       (zero? (rand-int 5)))
-        
-        apple-eaten (contains? apples new-head)
-        old-score   (:score state)
-        new-score   (if apple-eaten
-                      (inc old-score)
-                      old-score)
-        
-        new-wall    (if (and
-                          (not= old-score new-score)
-                          (zero? (mod new-score 3)))
-                      (generate-wall)
-                      (:wall state))
-        
-        
-        new-state  (-> state
-                       (update-in [:snake] #(cons
-                                              new-head
-                                              (if is-growing
-                                                %
-                                                (drop-last %))))
-                       (update-in [:growing] (fn [g]
-                                               (if is-growing
-                                                 (dec g)
-                                                 g)))
-                       (assoc-in [:last-direction] direction)
-                       (assoc-in [:wall] new-wall)
-                       (assoc-in [:score] new-score)
-                       
-                       (update-in [:apples] #(if apple-eaten
-                                               (disj % new-head)
-                                               %))
-                       
-                       (update-in [:growing] #(if apple-eaten
-                                                (+ % 2)
-                                                %))
-                       
-                       (add-apple adding-apple))
-        
-        
-        
-        old-snake  (:snake state)
-        new-snake  (:snake new-state)
-        wall       (:wall  state)]
-    ; (println "new-state:" new-state)
-    (if (> new-score @high-score)
-      (reset! high-score new-score))
-    ; Check if the snake has hit itself
-    ; reset the game if it has
-    ; (if (not=
-    ;       (count (set new-snake))
-    ;       (count new-snake))
-    (if apple-eaten
-      new-state
-      (if (contains? (set (rest new-snake)) new-head)
-        (init-state)
-        ; check if the snake has hit the wall
-        ; reset the game if it has
-        (if (contains? (set wall) new-head)
-          (init-state)
-          new-state)))))
+        new-x           (case direction
+                          :left  (mod (- head-x 10) (q/width))
+                          :right (mod (+ head-x 10) (q/width))
+                          head-x)
+        new-y           (case direction
+                          :up   (mod (- head-y 10) (q/height))
+                          :down (mod (+ head-y 10) (q/height))
+                          head-y)        
+        is-growing      (> (:growing state) 0)
+        new-head        [new-x new-y]                
+        new-tail        (if is-growing
+                          (:snake state)
+                          (drop-last (:snake state)))
+        wall            (:wall  state)
+        new-state       (if (or
+                              (contains? (set new-tail) new-head)
+                              (contains? wall new-head))
+                          (init-state)
+                          (->
+                            (if is-growing
+                              (update state :growing dec)
+                              state)
+                            (assoc :last-direction direction)
+                            (update-apple-state new-head)
+                            (move-snake new-head new-tail)))]
+    new-state))
+
+
+
 
 (defn handle-keypress [state event]
-  ; (println "event:" event)
   (let [key (:key event)]
     (if (and
           (contains? directions key)
@@ -293,10 +296,10 @@
       state)))
 
 
-(defn run [title]
+(defn run []
   (q/sketch 
     :size [500 300]
-    :title title
+    :title "Snake Game!"
     :setup setup
     :draw draw
     :update update-state
@@ -304,5 +307,5 @@
     :middleware [m/fun-mode]))
 
 (defn -main [& args]
-  (run "Snake Game"))
+  (run))
 
